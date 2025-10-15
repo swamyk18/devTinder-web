28be79dff01163653324bd53acb394aed1024851
swamyk18 <swamyswamy62162@gmail.com>
2025-10-15 20:39:38 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 
 function App() {
@@ -6,7 +10,25 @@ function App() {
 
   return (
     <>
-     
+        <BrowserRouter basename="/">
+         <Routes>
+            <Route path="/" element={<Body/>}> 
+            
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/Profile" element={<Profile/>}></Route>
+
+            
+            
+            </Route>
+         
+         
+         </Routes>
+        </BrowserRouter>
+
+
+
+        {/* <NavBar/> */}
+        {/* <h1 className=" text-3xl font-bold underline">hello world</h1> */}
     </>
   )
 }
